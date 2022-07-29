e66f7d7f6305eaec823540b55799a4510a7e8f36
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-29 15:13:00 +0300

    Add Task 016

---
+++ Seminary/sem2_new/Task016/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа: ");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+            int Square1 = Num1*Num1;
+            int Square2 = Num2*Num2;
+
+            if(Num1==Square2 || Num2==Square1) Console.WriteLine("Одно из введенных чисел является квадратом второго.");
+            else Console.WriteLine("Ни одно из введенных чисел не является квадратом второго.");
+        }
+    }
+}
