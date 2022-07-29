54d722a1917252e10651aa45163c8dbedd028f9e
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-29 14:56:39 +0300

    Add Task 13

---
+++ Seminary/sem2_new/Task013/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            double Num = Convert.ToInt32(Console.ReadLine());
+            double Div1 = Num % 7;
+            double Div2 = Num % 23;
+
+            if(Div1==0 && Div2==0)
+            {
+                Console.WriteLine("Введенное число кратно одновременно двум числам 7 и/или 23. ");
+            }
+            else
+            {
+                Console.WriteLine("Введенное число не кратно одновременно двум числам 7 и 23. ");
+            }
+        }
+    }
+}
