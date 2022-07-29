ccec2fbfa8bee89e3ac9aba31256d2ba47cfec41
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-29 14:13:09 +0300

    Add Task 012

---
+++ Seminary/sem2_new/Task012/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            double Num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double Num2 = Convert.ToInt32(Console.ReadLine()); 
+
+            double Div = Num1/Num2;
+            double remains = Num1%Num2;
+           // Console.WriteLine(Div);
+           // Console.WriteLine(remains);
+
+
+            if(remains == 0)
+            {
+                Console.WriteLine($"Число {Num2} кратно {Num1}");
+            }
+            else
+            {
+                Console.WriteLine($"Остаток {remains}");
+            }
+        }
+    }
+}
