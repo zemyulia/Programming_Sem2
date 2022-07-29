3bf2abc7dd2f11dc355babe5d224281c9338dd1c
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-29 13:48:33 +0300

    Add Task 011

---
+++ Seminary/sem2_new/Task011/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = new Random().Next(100,1000);
+            Console.WriteLine($"Случайное число {Num}");
+
+            
+            int N = (Num/100)*10 + (Num%10); 
+           
+            Console.WriteLine($"Преобразованное число: {N}");
+
+        }
+    }
+}
+
+
