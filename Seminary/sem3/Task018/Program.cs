f5eedd4554438110dcbce7a0b8c59b788b988d17
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-30 14:25:20 +0300

    Add Task 018

---
+++ Seminary/sem3/Task018/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число - номер четверти в декартовой плоскости, в диапазоне от 1 до 4: ");
+            int coordinate = Convert.ToInt32(Console.ReadLine());
+
+
+            if(coordinate == 1) Console.WriteLine("Для первой четверти координаты X>0, Y>0.");
+            if(coordinate == 2) Console.WriteLine("Для второй четверти координаты X<0, Y>0.");
+            if(coordinate == 3) Console.WriteLine("Для третьей четверти координаты X<0, Y<0.");
+            if(coordinate == 4) Console.WriteLine("Для четвертой четверти координаты X>0, Y<0.");
+
+        }
+    }
+}
