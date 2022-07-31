68a79edcd42a6c056731b58a4767df5ca83392b3
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-31 13:34:49 +0300

    Add Homework Task 019

---
+++ Seminary/sem3/Homework/Task019/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 5-значное число: ");
+            int MyNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] Digits = new int[5];
+
+            if (MyNumber<100000 && MyNumber>9999)
+            {
+                for(int i = 4; i>=0; i = i-1)
+                {
+                    Digits[i] = MyNumber%10;
+                    MyNumber = MyNumber/10;
+                }
+
+                if(Digits[0]==Digits[4] && Digits[1]==Digits[3])
+                {
+                    Console.WriteLine("Число является полиндромом.");
+                }
+                else Console.WriteLine("Число не является полиндромом.");
+                
+            }
+            else Console.WriteLine("Введенное число не соответствует условию задачи.");
+
+            
+        }
+    }
+}
