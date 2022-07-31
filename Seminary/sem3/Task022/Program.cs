6642fae41e0815b002becb71fef11a2b04959301
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-31 12:37:37 +0300

    Add Task 022 - table of squares.

---
+++ Seminary/sem3/Task022/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Введите число:");
+                int MyNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write($"Таблица квадратов числа {MyNumber}: |");
+                int[] result = new int[MyNumber];
+
+                for(int i=0; i<MyNumber; i++)
+                {
+                    result[i] = (i+1)*(i+1);
+                    Console.Write($" {result[i]}|");
+
+                }
+            
+        }
+    }
+}
+
