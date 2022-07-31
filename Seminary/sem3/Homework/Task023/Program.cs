7f49af477bc442c379d22c161402190b14ba928c
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-31 14:18:52 +0300

    Add Homework Task023.

---
+++ Seminary/sem3/Homework/Task023/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Задача 23
+            // Напишите программу, которая принимает на вход число (N)
+            //  и выдаёт таблицу кубов чисел от 1 до N.
+            // 3 -> 1, 8, 27
+            // 5 -> 1, 8, 27, 64, 125
+
+            Console.WriteLine("Введите число: ");
+            int MyNum = Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+            Console.Write("Таблица кубов данного числа: |");
+
+            for(int i = 0; i<=MyNum; i++)
+            {
+                res = i*i*i;
+                Console.Write($"{res} |");
+            }
+
+        }
+    }
+}
